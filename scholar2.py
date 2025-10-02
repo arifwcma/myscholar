dd47261bf773c1948b0c24e9a6eb150d5a7257c1
arif.rahman <arif.rahman@wcma.vic.gov.au>
2025-10-02 14:55:40 +1000

    done

--- scholar2.py
+++ scholar2.py
@@ -3,7 +3,7 @@ from scholarly import scholarly
 author = scholarly.search_author_id("2546GSEAAAAJ")
 author = scholarly.fill(author, sections=["publications"])
 
-with open("titles.txt", "w", encoding="utf-8") as f:
+with open("titles2.txt", "w", encoding="utf-8") as f:
     for pub in author["publications"]:
         pub_filled = scholarly.fill(pub)
         title = pub_filled.get("bib", {}).get("title", "")
