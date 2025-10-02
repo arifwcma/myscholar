94bde0386059f326a0c196364ab5a024a1003a56
arif.rahman <arif.rahman@wcma.vic.gov.au>
2025-10-02 15:00:06 +1000

    done

--- scholar3.py
+++ scholar3.py
@@ -1,10 +1,13 @@
 from scholarly import scholarly
 
-author = scholarly.search_author_id("dfBU3voAAAAJ")
+author = scholarly.search_author_id("_50HOBwAAAAJ")
 author = scholarly.fill(author, sections=["publications"])
 
-with open("titles2.txt", "w", encoding="utf-8") as f:
+with open("titles.txt", "w", encoding="utf-8") as f:
     for pub in author["publications"]:
-        pub_filled = scholarly.fill(pub)
-        title = pub_filled.get("bib", {}).get("title", "")
-        f.write(title + "\n\n")
+        try:
+            pub_filled = scholarly.fill(pub)
+            title = pub_filled.get("bib", {}).get("title", "")
+            f.write(title + "\n\n")
+        except Exception:
+            continue
